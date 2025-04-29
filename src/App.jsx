9e20353219b57b444eb77d33f170cb0940a5060f
Jaskaran-ST <jaskaran.singh02@st.com>
2025-04-29 18:43:48 +0530

    FE changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React,{ useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import authService from './appwrite/auth';
 import { login, logout } from './store/authSlice'
@@ -31,7 +31,7 @@ function App() {
       <div className='w-full block'>
         <Header />
         <main>
-          TODO: <Outlet />
+          <Outlet />
         </main>
         <Footer />
       </div>
