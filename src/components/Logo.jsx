1ee3ad2ee465f85832550391d72ee5033896418c
Jaskaran-ST <jaskaran.singh02@st.com>
2025-04-29 22:17:11 +0530

    updated logo width

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo({ width = '100px' }) {
     return (
         <div>
-            <img src="/download.png" alt="logo" width={width} />
+            <img src="/download.png" alt="logo" />
         </div>
     )
 }
