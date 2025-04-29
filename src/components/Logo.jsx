15e8f31db63c983958305e2ea4180b3338071aeb
Jaskaran-ST <jaskaran.singh02@st.com>
2025-04-29 22:28:32 +0530

    added path of images

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import logoImage from '../images/download.png';
 
 function Logo({ width = '100px' }) {
     return (
         <div>
-            <img src="images/download.png" alt="logo" />
+            <img src={logoImage} alt="logo" />
         </div>
     )
 }
