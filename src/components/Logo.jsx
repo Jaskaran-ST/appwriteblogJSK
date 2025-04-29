2750a12e7a43a0375339fa911d11d238b459d1dc
Jaskaran-ST <jaskaran.singh02@st.com>
2025-04-29 18:50:45 +0530

    updated logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo({ width = '100px' }) {
     return (
         <div>
-            <img src="../../public/download (1).png" alt="logo" width={width} />
+            <img src="/download.png" alt="logo" width={width} />
         </div>
     )
 }
